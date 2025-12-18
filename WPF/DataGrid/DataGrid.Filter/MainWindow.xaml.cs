a307d9d488f471309ac75fbe405e5cdcbd137acd
MissBlue <magicguner124@gmail.com>
2025-12-18 23:04:46 +0800

    DataGrid数据筛选功能，MVVM实现

--- WPF/DataGrid/DataGrid.Filter/MainWindow.xaml.cs
+++ WPF/DataGrid/DataGrid.Filter/MainWindow.xaml.cs
@@ -1,36 +1,11 @@
-﻿using System.ComponentModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-
-namespace DataGrid.Filter;
+﻿namespace DataGrid.Filter;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow {
-    private readonly List<Employee>  _employees;
-    private readonly ICollectionView _collectionView;
-
     public MainWindow() {
         InitializeComponent();
-        _employees = Employee.FakeMany(10).ToList();
-        _collectionView = CollectionViewSource.GetDefaultView(_employees);
-        DataGrid.ItemsSource = _collectionView;
-        _collectionView.Filter = item => {
-                                     if (item is not Employee employee) return false;
-                                     var key = FilterTextBox.Text;
-                                     if (employee.FirstName == null || employee.LastName == null) return false;
-                                     return employee.FirstName.Contains(key) || employee.LastName.Contains(key);
-                                 };
-    }
-
-    private void FilterTextBox_OnTextChanged(object sender, TextChangedEventArgs e) {
-        _collectionView.Refresh();
-    }
-
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
-        _employees.Add(Employee.FakeOne());
-        _collectionView.Refresh();
+        DataContext = new MainWindowViewModel();
     }
 }
---
+++ WPF/DataGrid/DataGrid.Filter/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DataGrid.Filter;
+
+public partial class MainWindowViewModel : ObservableObject {
+    [ObservableProperty]
+    private List<Employee> _employees;
+
+    [ObservableProperty]
+    private ICollectionView _employeeCollection;
+
+    [ObservableProperty]
+    private string? _key;
+
+    public MainWindowViewModel() {
+        Employees = Employee.FakeMany(10).ToList();
+        EmployeeCollection = CollectionViewSource.GetDefaultView(Employees);
+        EmployeeCollection.Filter = (item => {
+                                         if (string.IsNullOrEmpty(Key)) return true;
+                                         if (item is not Employee employee) return false;
+                                         if (employee.FirstName == null || employee.LastName == null) return false;
+                                         return employee.FirstName.Contains(Key) || employee.LastName.Contains(Key);
+                                     });
+    }
+
+    [RelayCommand]
+    private void AddEmployee() {
+        Employees.Add(Employee.FakeOne());
+        EmployeeCollection.Refresh();
+    }
+
+    partial void OnKeyChanged(string? value) {
+        EmployeeCollection.Refresh();
+    }
+}
