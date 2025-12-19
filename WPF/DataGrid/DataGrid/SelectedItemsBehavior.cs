087da8157399f0873c9ee213c959ebd2e742eead
MissBlue <magicguner124@gmail.com>
2025-12-19 15:12:47 +0800

    在Behavior中绑定依赖属性，绑定SelectedItems

---
+++ WPF/DataGrid/DataGrid/SelectedItemsBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Microsoft.Xaml.Behaviors;
+
+namespace DataGrid;
+
+public class SelectedItemsBehavior : Behavior<MultiSelector> {
+    protected override void OnAttached() {
+        base.OnAttached();
+        AssociatedObject.SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+        var selector = (MultiSelector)sender;
+        BindableSelectedItems = selector.SelectedItems;
+        selector.GetBindingExpression(BindableSelectedItemsProperty)?.UpdateSource();
+    }
+
+    protected override void OnDetaching() {
+        base.OnDetaching();
+        AssociatedObject.SelectionChanged -= OnSelectionChanged;
+    }
+
+    #region Bindable SelectedItems
+
+    public static readonly DependencyProperty BindableSelectedItemsProperty
+        = DependencyProperty.Register(nameof(BindableSelectedItems), typeof(IList), typeof(SelectedItemsBehavior), new PropertyMetadata(null));
+
+    public IList BindableSelectedItems {
+        get => (IList)GetValue(BindableSelectedItemsProperty);
+        set => SetValue(BindableSelectedItemsProperty, value);
+    }
+
+    #endregion
+}
