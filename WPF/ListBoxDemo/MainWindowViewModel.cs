88e4eb08190e3eb03fa8c8cd134df974cf12640e
MissBlue <magicguner124@gmail.com>
2025-12-22 03:06:29 +0800

    ListBox

---
+++ WPF/ListBoxDemo/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ListBoxDemo;
+
+public partial class MainWindowViewModel : ObservableObject {
+    public List<Student> Students { get; } = [
+                                                 new(FullName: "Jeremy", Age: 17),
+                                                 new(FullName: "Brian", Age: 19),
+                                                 new(FullName: "Tom", Age: 18),
+                                                 new(FullName: "Peter", Age: 20),
+                                                 new(FullName: "Diana", Age: 19),
+                                                 new(FullName: "Emma", Age: 18)
+                                             ];
+
+    [ObservableProperty]
+    private string? _selectedName;
+
+    [RelayCommand]
+    private void SelectStudent(string name) {
+        SelectedName = name;
+    }
+}
+
+public record Student(string FullName, int Age);
