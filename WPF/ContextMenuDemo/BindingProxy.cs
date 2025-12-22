b1e952051ee60c6ec2917859a59df729b9dc5ab2
MissBlue <magicguner124@gmail.com>
2025-12-22 23:39:03 +0800

    ContextMenu

---
+++ WPF/ContextMenuDemo/BindingProxy.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ContextMenuDemo;
+
+public class BindingProxy : Freezable {
+    protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+    public static readonly DependencyProperty DataProperty = DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new PropertyMetadata(default(object)));
+
+    public object Data {
+        get => GetValue(DataProperty);
+        set => SetValue(DataProperty, value);
+    }
+}
---
+++ WPF/ContextMenuDemo/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ContextMenuDemo;
+
+public partial class MainWindowViewModel : ObservableObject {
+    [ObservableProperty]
+    private List<string> _items = ["MissBlue", "MagicGunner",];
+
+    [RelayCommand]
+    private void Foo(object obj) {
+        // Do nothing
+        MessageBox.Show($"点击了{obj}");
+    }
+}
